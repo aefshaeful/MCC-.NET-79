9db99fcbafc80982179bc397066eedc8fbaecbdd
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-17 08:26:27 +0700

    Update ViewMenu.cs

--- Connectivity_SQL/ViewMenu.cs
+++ Connectivity_SQL/ViewMenu.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Connectivity_SQL
 {
-    internal class ViewMenu
+    public class ViewMenu
     {
+        SqlConnection conec = Connection.SqlConnection();
+        Console.WriteLine("=== Database Employees ===");
+        Console.WriteLine("1. Reg);
     }
 }
