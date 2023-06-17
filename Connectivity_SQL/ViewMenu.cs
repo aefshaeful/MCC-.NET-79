812869a4e4ec52f725da8f4ba4cf388657ed8a81
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-17 09:30:53 +0700

    Update ViewMenu.cs

--- Connectivity_SQL/ViewMenu.cs
+++ Connectivity_SQL/ViewMenu.cs
@@ -5,13 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using Connectivity_SQLQuery;
 
 namespace Connectivity_SQL
 {
     public class ViewMenu
     {
-        SqlConnection conec = Connection.SqlConnection();
+        SqlConnection connec = new SqlConnection();
+        connection = Connection.connection;
+        Regions _region = new Regions();
+        Countries _countrie = new Countries();
+        Locations _location = new Locations();
+        Departments _department = new Departments();
+        Employees _employee = new Employees();
+        Histories _historie = new Histories();
+        Jobs _job = new Jobs();
+
+
+
+
         Console.WriteLine("=== Database Employees ===");
-        Console.WriteLine("1. Reg);
+        Console.WriteLine("1. Reg");
     }
 }
