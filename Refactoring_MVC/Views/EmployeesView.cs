4e5fdc8a1f97c2accf2a61c444caee8dbc1776b3
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 04:29:07 +0700

    Create EmployeesView.cs

---
+++ Refactoring_MVC/Views/EmployeesView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Controllers;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Views
+{
+    public class EmployeesView
+    {
+        public void GetAll(List<Employees> employees)
+        {
+            ViewMenuController viewMenu = new ViewMenuController();
+            Console.Clear();
+            Console.WriteLine("\tGET ALL EMPLOYEES\t");
+            Console.WriteLine("===================================");
+            
+            foreach (Employees employ in employees)
+            {
+                Console.WriteLine($"Id: {employ.Id} Firsh Name: {employ.FirshName} Last Name: {employ.LastName} Email: {employ.Email} Phone Number: {employ.PhoneNumber} Hire Date: {employ.HireDate} Salary: {employ.Salary} Commision Pct: {employ.CommisionPct} Manager Id: {employ.ManagerId} Job Id: {employ.JobId} Department Id: {employ.DepartmentId}");
+            }
+            Console.ReadKey();
+            viewMenu.View();
+        }
+    }
+}
