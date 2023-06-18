7083b06d1c212dcbf06d83c9d654064dda46d422
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-18 16:00:20 +0700

    Create RegionsView.cs

---
+++ Refactoring_MVC/Views/RegionsView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+Refactoring_MVC.Models
+
+namespace Refactoring_MVC.Views
+{
+    public class RegionsView
+    {
+        public void GetById(Regions regions)
+        {
+            Console.WriteLine($"Id: {region.Id} Name: {region.Name}");
+            /*Console.ReadKey();*/
+        }
+    }
+}
