99f7ff9cf33b06a7398e25cf63742f240bded26e
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 01:49:00 +0700

    Create CountriesView.cs

---
+++ Refactoring_MVC/Views/CountriesView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Views
+{
+    public class CountriesView
+    {
+        public void GetById(Countries country)
+        {
+            Console.WriteLine($"Id: {country.Id} Name: {country.Name} RegionId: {country.RegionId}");
+        }
+
+        public void GetAll(List<Countries> countries)
+        {
+            foreach (Countries country in countries)
+            {
+                GetById(country);
+            }
+        }
+
+        public void NotFound()
+        {
+            Console.WriteLine("Data Not Found!");
+        }
+    }
+}
