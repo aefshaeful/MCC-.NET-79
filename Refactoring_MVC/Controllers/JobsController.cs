33c2902b1796113ef1973639a2ccc315c8df1d4a
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 04:28:03 +0700

    Create JobsController.cs

---
+++ Refactoring_MVC/Controllers/JobsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Views;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Controllers
+{
+    public class JobsController
+    {
+        private Jobs _jobs = new Jobs();
+        private JobsView _jobsview = new JobsView();
+
+        public void ViewMenuJobs()
+        {
+            var jobs = _jobs.GetAll();
+            _jobsview.GetAll(jobs);
+        }
+    }
+}
