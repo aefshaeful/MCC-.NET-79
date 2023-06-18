2566cd5edb3b8721b84d254377b4970242b12ef5
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 04:26:31 +0700

    Create DepartmentsController.cs

---
+++ Refactoring_MVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Views;
+using Refactoring_MVC.Models;
+
+
+namespace Refactoring_MVC.Controllers
+{
+    public class DepartmentsController
+    {
+        private Departments _departments = new Departments();
+        private DepartmentsView _departmentsview = new DepartmentsView();
+
+        public void ViewMenuDepartments()
+        {
+            //GETALL : DEPARTMENTS
+            var departments = _departments.GetAll();
+            _departmentsview.GetAll(departments);
+        }
+
+    }
+}
