4cbccf9177f631b3518e469b357d2dbfaa91cf5d
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 02:47:56 +0700

    Create LocationsController.cs

---
+++ Refactoring_MVC/Controllers/LocationsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Views;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Controllers
+{
+    public class LocationsController
+    {
+        private Locations _locations = new Locations();
+        private LocationsView _locationsview = new LocationsView();
+
+        // VIEW MENU LOCATION
+        public void ViewMenuLocation()
+        {
+            // GETALL LOCATION
+           /* ViewMenuController viewMenu = new ViewMenuController();
+            Console.Clear();
+            Console.WriteLine("\tGET ALL LOCATION\t");
+            Console.WriteLine("===================================");*/
+            
+            var locations = _locations.GetAll();
+            _locationsview.GetAll(locations);
+
+            /*Console.ReadKey();
+            viewMenu.View();*/
+        }
+    }
+}
