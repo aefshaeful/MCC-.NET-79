a5fa142b15de5dd48d9d3d767cb6e57b64ab2749
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 04:27:52 +0700

    Create HistoriesController.cs

---
+++ Refactoring_MVC/Controllers/HistoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Views;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Controllers
+{
+    public class HistoriesController
+    {
+        private Histories _histories = new Histories();
+        private HitroriesView _historiesview = new HitroriesView();
+
+        public void ViewMenuHistories()
+        {
+            //GETALL : HISTORIES
+            var histories = _histories.GetAll();
+            _historiesview.GetAll(histories);
+        }
+    }
+}
