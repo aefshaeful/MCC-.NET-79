18cbe400a7442c0c19a44041994c4762ce3a5020
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 04:28:50 +0700

    Create DepartmentsView.cs

---
+++ Refactoring_MVC/Views/DepartmentsView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Controllers;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Views
+{
+    public class DepartmentsView
+    {
+        public void GetAll(List<Departments> departments)
+        {
+            ViewMenuController viewMenu = new ViewMenuController();
+            Console.Clear();
+            Console.WriteLine("\tGET ALL DEPARTMENTS");
+            Console.WriteLine("===================================");
+            
+            foreach (Departments depart in departments)
+            {
+                Console.WriteLine($"Id: {depart.Id} Name: {depart.Name} Location Id: {depart.LocationId} Manager Id: {depart.ManagerId}");
+            }
+            Console.ReadKey();
+            viewMenu.View();
+        }
+    }
+}
