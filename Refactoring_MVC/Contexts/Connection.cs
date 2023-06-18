89f1a02eeb6a1c6039bd3fafe638efebdd02337c
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-18 13:46:52 +0700

    Update Connection.cs

--- Refactoring_MVC/Contexts/Connection.cs
+++ Refactoring_MVC/Contexts/Connection.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Refactoring_MVC.Contexts
 {
-    internal class Connection
+    public class Connection
     {
+        // Connection Secara Global
+        public static string connectionString = "Data Source=LAPTOP-8IGJNOSS;Database=db_hr;Integrated Security=True;Connect Timeout=30;";
+        public static SqlConnection connection = new SqlConnection(connectionString);
     }
 }
