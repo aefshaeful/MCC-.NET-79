a5c7e0b1e2a60adfddee153f3685e8d02421a7e8
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 02:48:38 +0700

    Create LocationsView.cs

---
+++ Refactoring_MVC/Views/LocationsView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Controllers;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Views
+{
+    public class LocationsView
+    {
+        public void GetAll(List<Locations> locations)
+        {
+            ViewMenuController viewMenu = new ViewMenuController();
+            Console.Clear();
+            Console.WriteLine("\tGET ALL LOCATION\t");
+            Console.WriteLine("===================================");
+
+            foreach (Locations location in locations)
+            {
+                Console.WriteLine($"Id: {location.Id} StreetAddress: {location.StreetAddress} PostalCode: {location.PostalCode} City: {location.City} StateProvince: {location.StateProvince} CountryId: {location.CountryId}");
+            }
+
+            Console.ReadKey();
+            viewMenu.View();
+        }
+    }
+}
