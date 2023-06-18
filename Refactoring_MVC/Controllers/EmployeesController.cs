4ed7acb9c3abbdd31ad8e2dcce8ebf1259269d6f
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 04:27:38 +0700

    Create EmployeesController.cs

---
+++ Refactoring_MVC/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Views;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Controllers
+{
+    public class EmployeesController
+    {
+        private Employees _employees = new Employees();
+        private EmployeesView _employeesview = new EmployeesView();
+
+        public void ViewMenuEmployees()
+        {
+            //GETALL : EMPLOYEES
+            var employees = _employees.GetAll();
+            _employeesview.GetAll(employees);
+        }
+    }
+}
