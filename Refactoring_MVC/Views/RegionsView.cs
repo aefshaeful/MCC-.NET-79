29cfe2a95f9d804d3c2fdcdb5847a2e790a593a6
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-18 17:50:40 +0700

    Update RegionsView.cs

--- Refactoring_MVC/Views/RegionsView.cs
+++ Refactoring_MVC/Views/RegionsView.cs
@@ -9,10 +9,23 @@ namespace Refactoring_MVC.Views
 {
     public class RegionsView
     {
-        public void GetById(Regions regions)
+        public void GetById(Regions region)
         {
             Console.WriteLine($"Id: {region.Id} Name: {region.Name}");
             /*Console.ReadKey();*/
+        }
+
+        public void GetAll(List<Regions> regions)
+        {
+            foreach (Regions region in regions)
+            {
+                GetById(region);
+            }
+        }
+
+        public void NotFound()
+        {
+            Console.WriteLine("Data Not Found!");
         }
     }
 }
