f4fced157056b66a901492f5c7ae8dfe99cdaad3
aefshaeful <aefsaefulrohma042@gmail.com>
2023-06-19 07:01:47 +0700

    Create LINQView.cs

---
+++ Refactoring_MVC/Views/LINQView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoring_MVC.Controllers;
+using Refactoring_MVC.Models;
+
+namespace Refactoring_MVC.Views
+{
+    public class LINQView
+    {
+        /*public void MenuLinq()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\tVIEW MENU LINQ\t");
+            Console.WriteLine("===================================");
+            Console.WriteLine("1. Get Employees With Just Five ID");
+            Console.WriteLine("2. Total Employees of Each Department");
+            Console.WriteLine("3. Exit");
+            Console.Write("Please choose a menu linq :");
+        }
+
+
+        public void GetTakeEmployees()
+        {
+            LINQController linqconec = new LINQController();
+            foreach (var emp in employee)
+            {
+                Console.WriteLine("ID :" + emp.id);
+                Console.WriteLine("Full Name :" + emp.FullName);
+                Console.WriteLine("Email :" + emp.Email);
+                Console.WriteLine("Phone Number :" + emp.PhoneNumber);
+                Console.WriteLine("Salary :" + emp.Salary);
+                Console.WriteLine("Departments Name :" + emp.DepartmentsName);
+                Console.WriteLine("Street Address :" + emp.StreetAddress);
+                Console.WriteLine("Country Name :" + emp.CountryName);
+                Console.WriteLine("Regions :" + emp.Regions);
+                Console.WriteLine("======================================");
+            }
+            Console.WriteLine("Press enter to continue");
+            Console.ReadKey();
+            this.ViewMenuLinq();
+        }
+    
+
+        public void GetTolalEmployeesSetDepart()
+        {
+            LINQController linqconec = new LINQController();
+            foreach (var em in employ)
+            {
+                Console.WriteLine("Department Name :" + em.DepartmentsName);
+                Console.WriteLine("Total Employees :" + em.TotalEmployees);
+                Console.WriteLine("Min Salary :" + em.MinSalary);
+                Console.WriteLine("Max Salary :" + em.MaxSalary);
+                Console.WriteLine("Average Salary :" + em.AverageSalary);
+                Console.WriteLine("======================================");
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine("Press enter to continue");
+            Console.ReadKey();
+            this.ViewMenuLinq();
+        }*/
+
+
+    }
+}
